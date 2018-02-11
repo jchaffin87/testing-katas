2dbef9ac8b4cbb2f2943b23cf8b5c86ae8db2d71
jchaffin87 <jchaffin87@gmail.com>
2018-02-11 15:21:27 -0500

    added kata instructions comment to CountXXer

--- src/main/java/com/jchaffin/testingkatas/CountXXer.java
+++ src/main/java/com/jchaffin/testingkatas/CountXXer.java
@@ -2,6 +2,9 @@ package com.jchaffin.testingkatas;
 
 public class CountXXer {
 
+	// Count the number of "xx" in the given string. We'll say that overlapping is
+	// allowed, so "xxx" contains 2 "xx".
+
 	public int countXX(String str) {
 		return 1;
 	}
