4f191530cf6b7ca0e5d3fe1eb801a78f194538c8
jchaffin87 <jchaffin87@gmail.com>
2018-02-11 15:03:47 -0500

    added test to check for errors when frontTimes is passed and empty string

--- src/test/java/com/jchaffin/testingkatas/FrontTimesTest.java
+++ src/test/java/com/jchaffin/testingkatas/FrontTimesTest.java
@@ -2,26 +2,36 @@ package com.jchaffin.testingkatas;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FrontTimesTest {
 
+	FrontTimeser underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new FrontTimeser();
+	}
+
 	@Test
 	public void whenFrontTimesIsPassedAnAAndAThreeItReturnsAAA() {
-		FrontTimeser underTest = new FrontTimeser();
 		assertEquals("AAA", underTest.frontTimes("A", 3));
 	}
 
 	@Test
 	public void whenFrontTimesIsPassedBAndTwoItReturnsBB() {
-		FrontTimeser underTest = new FrontTimeser();
 		assertEquals("BB", underTest.frontTimes("B", 2));
 	}
 
 	@Test
 	public void whenFrontTimesIsPassedABCDAndThreeItReturnsABCABCABC() {
-		FrontTimeser underTest = new FrontTimeser();
 		assertEquals("ABCABCABC", underTest.frontTimes("ABCD", 3));
+	}
+
+	@Test
+	public void whenFrontTimesIsPassedAnEmptyStringAndThreeItReturnsAnEmptyString() {
+		assertEquals("", underTest.frontTimes("", 3));
 	}
 
 }
