3ab753ff43afae7d4ffbbf0716347f471c270caa
jchaffin87 <jchaffin87@gmail.com>
2018-03-20 16:44:37 -0400

    two additional tests written for stringBits method. stringBits kata complete.

--- src/test/java/com/jchaffin/testingkatas/StringBitsTest.java
+++ src/test/java/com/jchaffin/testingkatas/StringBitsTest.java
@@ -7,21 +7,33 @@ import org.junit.Test;
 public class StringBitsTest {
 
 	@Test
-	public void stringBitsReturnsHWHenPassedH() {
+	public void stringBitsReturnsHWhenPassedH() {
 		StringBits underTest = new StringBits();
 		assertEquals("H", underTest.stringBits("H"));
 	}
 
 	@Test
-	public void stringBitsReturnsHiWHenPassedHi() {
+	public void stringBitsReturnsHWhenPassedHi() {
 		StringBits underTest = new StringBits();
 		assertEquals("H", underTest.stringBits("Hi"));
 	}
 
 	@Test
-	public void stringBitsReturnsACEWHenPassedABCDE() {
+	public void stringBitsReturnsACEWhenPassedABCDE() {
 		StringBits underTest = new StringBits();
 		assertEquals("ACE", underTest.stringBits("ABCDE"));
+	}
+
+	@Test
+	public void stringBitsReturnsHLOWhenPassedHELLO() {
+		StringBits underTest = new StringBits();
+		assertEquals("HLO", underTest.stringBits("HELLO"));
+	}
+
+	@Test
+	public void stringBitsReturnsHELLOWhenPassedHEEOLOLEO() {
+		StringBits underTest = new StringBits();
+		assertEquals("HELLO", underTest.stringBits("HEEOLOLEO"));
 	}
 
 }
