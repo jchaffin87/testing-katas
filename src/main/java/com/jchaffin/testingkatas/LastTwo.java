b2705a4b60ea0640ff4c89e6f67afdbb34eb353e
jchaffin87 <jchaffin87@gmail.com>
2018-03-22 12:55:44 -0400

    LastTwoTest class created. Initial test for lastTwo method written. LastTwo class created. lastTwo method created.

---
+++ src/main/java/com/jchaffin/testingkatas/LastTwo.java
@@ -0,0 +1,16 @@
+package com.jchaffin.testingkatas;
+
+public class LastTwo {
+
+	// Given a string, return the count of the number of times that a substring
+	// length 2 appears in the string and also as the last 2 chars of the string, so
+	// "hixxxhi" yields 1 (we won't count the end substring).
+
+	// make a string of the last two chars and count how many times that string
+	// appears in str.
+
+	public int lastTwo(String str) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/jchaffin/testingkatas/LastTwoTest.java
@@ -0,0 +1,22 @@
+package com.jchaffin.testingkatas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LastTwoTest {
+
+	LastTwo underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new LastTwo();
+	}
+
+	@Test
+	public void lastTwoReturnsZeroWhenPassedA() {
+		assertEquals(0, underTest.lastTwo("A"));
+	}
+
+}
