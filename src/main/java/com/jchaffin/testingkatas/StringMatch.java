5558963ee38bf2d05d3a235a9a87ef9395ba80c7
jchaffin87 <jchaffin87@gmail.com>
2018-04-06 12:55:58 -0400

    StringMatchTest class created. StringMatch class created. Initial test for stringMatch method written. stringMatch method created.

---
+++ src/main/java/com/jchaffin/testingkatas/StringMatch.java
@@ -0,0 +1,9 @@
+package com.jchaffin.testingkatas;
+
+public class StringMatch {
+
+	public int stringMatch(String stringA, String stringB) {
+		return 1;
+	}
+
+}
---
+++ src/test/java/com/jchaffin/testingkatas/StringMatchTest.java
@@ -0,0 +1,22 @@
+package com.jchaffin.testingkatas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringMatchTest {
+
+	StringMatch underTest;
+
+	@Before
+	public void setUp() throws Exception {
+		underTest = new StringMatch();
+	}
+
+	@Test
+	public void stringMatchReturnsOneWhenPassedHeAndHe() {
+		assertEquals(1, underTest.stringMatch("he", "he"));
+	}
+
+}
