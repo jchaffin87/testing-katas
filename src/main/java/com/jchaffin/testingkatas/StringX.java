6cd4163e0f888cafc9fadbe14b0e24cb64d348b3
jchaffin87 <jchaffin87@gmail.com>
2018-04-12 10:43:56 -0400

    StringXTest class created. Initial test written for removeXs method. StringX class created. removeXs method created. First two tests written.

---
+++ src/main/java/com/jchaffin/testingkatas/StringX.java
@@ -0,0 +1,9 @@
+package com.jchaffin.testingkatas;
+
+public class StringX {
+
+	public String removeXs(String str) {
+		return str;
+	}
+
+}
---
+++ src/test/java/com/jchaffin/testingkatas/StringXTest.java
@@ -0,0 +1,32 @@
+package com.jchaffin.testingkatas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringXTest {
+
+	StringX underTest;
+
+	@Before
+	public void setUp() throws Exception {
+		underTest = new StringX();
+	}
+
+	@Test
+	public void removeXsMethodReturnsEmptyStringWhenPassedEmptyString() {
+		assertEquals("", underTest.removeXs(""));
+	}
+
+	@Test
+	public void removeXsMethodReturnsXStringWhenPassedX() {
+		assertEquals("X", underTest.removeXs("X"));
+	}
+
+	@Test
+	public void removeXsMethodReturnsXWhenPassedXX() {
+		assertEquals("X", underTest.removeXs("XX"));
+	}
+
+}
